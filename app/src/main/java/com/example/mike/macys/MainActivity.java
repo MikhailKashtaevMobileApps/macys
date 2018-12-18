3f28add9e02c290c0a82ccdafc4642c4937cc9f8
MikhailKashtaevMobileApps <mikhail.kashtaev@mobileappscompany.us>
2018-12-17 20:07:54 -0500

    added navigation

--- app/src/main/java/com/example/mike/macys/MainActivity.java
+++ app/src/main/java/com/example/mike/macys/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.mike.macys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mike.macys.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/mike/macys/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.mike.macys.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+
+
+}
